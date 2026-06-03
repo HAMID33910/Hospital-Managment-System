8d4ee981bb99adc657a42dee4ff288d6ddd6d819
HAMID33910 <thehamidbadshadon@gmail.com>
2026-06-03 15:52:21 +0500

    update

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -36,13 +36,45 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 class DoctorSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
-    user_id = serializers.IntegerField(write_only=True)
+    user_id = serializers.IntegerField(write_only=True, required=False)
+    doctor_name = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    username = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     class Meta:
         model = Doctor
-        fields = ['id', 'user', 'user_id', 'specialization', 'qualification',
+        fields = ['id', 'user', 'user_id', 'doctor_name', 'username', 'specialization', 'qualification',
                   'experience_years', 'consultation_fee', 'available_days',
                   'available_time_start', 'available_time_end', 'is_active']
+
+    def create(self, validated_data):
+        user_id = validated_data.pop('user_id', None)
+        doctor_name = validated_data.pop('doctor_name', None)
+        username = validated_data.pop('username', None)
+
+        if user_id:
+            user = User.objects.get(id=user_id)
+        elif doctor_name:
+            full_name = doctor_name.strip()
+            name_parts = full_name.split(' ', 1)
+            first_name = name_parts[0]
+            last_name = name_parts[1] if len(name_parts) > 1 else ''
+
+            if not username:
+                base_username = ''.join(ch for ch in full_name.lower() if ch.isalnum() or ch == ' ').replace(' ', '_')
+                username = base_username or f'doctor_{User.objects.count() + 1}'
+                counter = 1
+                while User.objects.filter(username=username).exists():
+                    username = f'{base_username}_{counter}'
+                    counter += 1
+
+            password = User.objects.make_random_password()
+            user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+            Profile.objects.filter(user=user).update(role='doctor')
+        else:
+            raise serializers.ValidationError({'user': 'Either user_id or doctor_name is required.'})
+
+        validated_data['user'] = user
+        return super().create(validated_data)
 
 
 class DoctorListSerializer(serializers.ModelSerializer):
